ee910764ad1253a7af7962f12535ccf53a197363
Ubuntu <ubuntu@ip-172-31-8-19.ap-south-1.compute.internal>
2022-07-20 20:04:50 +0000

    terraform module add

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = “AKIAXCCKN2A33MRMIJOG”
+}
+variable "aws_secret_key" {
+default = “Qs4gp7zVpGzUrjSlmecvmWJSMMwzX/DxwhMtC3Y4”
+ }
+variable "region" {
+    default = "ap-south-1"
+}
