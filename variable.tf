37d92e48079ac8d0733699c99c9666dab5b315b9
swapnil <swapnilgawali2404@gmail.com>
2022-07-21 02:36:43 +0530

    correction done

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-default = “AKIAXCCKN2A33MRMIJOG”
+default = "AKIAXCCKN2A33MRMIJOG"
 }
 variable "aws_secret_key" {
-default = “Qs4gp7zVpGzUrjSlmecvmWJSMMwzX/DxwhMtC3Y4”
+default = "Qs4gp7zVpGzUrjSlmecvmWJSMMwzX/DxwhMtC3Y4"
  }
 variable "region" {
     default = "ap-south-1"
